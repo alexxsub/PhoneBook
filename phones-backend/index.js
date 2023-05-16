90a32757953bd5b6c5351c7d6427f6b482347d79
aLexx Sub <alexei.s@mail.ru>
2023-05-16 22:13:06 +0300

    refactoring index

--- phones-backend/index.js
+++ phones-backend/index.js
@@ -2,31 +2,42 @@ import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 
 import mongoose from 'mongoose';
-
+//импортируем типы и резолвы
 import typeDefs from './types/index.js';
 import resolvers from './resolvers/index.js';
 
-//подключаемся к базе
-const MONGO_URI = 'mongodb://localhost:27017/PhoneBook'
-mongoose
-  .connect(MONGO_URI)
-  .then(() => console.log(`🚀  Database started ${MONGO_URI}`))
-  .catch(err => console.error(err))
+//Получаем конфигурацию из .env
+import dotenv from 'dotenv';
+dotenv.config();
+
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/PhoneBook',
+      HOST = process.env.HOST || 'localhost',
+      PORT = process.env.PORT || 9000;
+
+//подключение к базе
+async function ConnetMongoDB(){
+  console.log(`Connecting... ${MONGO_URI}`);  
+  await mongoose
+    .connect(MONGO_URI)
+    .then(() => console.log(`🚀 Have been connected ${MONGO_URI}`))
+};
 
 
-  //регистрируем схему и резолверы
-const server = new ApolloServer({
+
+  //создаем экземляр сервера
+
+async function StartApollo(){
+//регистрируем схему и резолверы, сохдаем экземпляр сервера
+  const server = new ApolloServer({
     typeDefs,
     resolvers
   });
 
-  //создаем экземляр сервера
-const HOST = process.argv[2];
-const PORT = process.argv[3];
-const { url } = await startStandaloneServer(server, {
-  listen: { host:HOST,port: PORT },
-});
+  await startStandaloneServer(server, {
+    listen: { host:HOST,port: PORT },
+  })
+  .then(({ url } ) =>console.log(`🚀  Apollo GraphQL server ready at: ${url}`));
+};
 
-console.log(`🚀  Server ready at: ${url}`);
-
-
+ConnetMongoDB().catch(err => console.error(err))
+StartApollo().catch(err => console.error(err))
