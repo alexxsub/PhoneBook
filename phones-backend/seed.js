2aa8e023088a80db8ed4650c0639592dda54e25d
alexx sub <alexei.s@list.ru>
2023-05-21 17:49:07 +0300

    seed

--- phones-backend/seed.js
+++ phones-backend/seed.js
@@ -5,7 +5,8 @@ import Phone from './models/Phone.js'
 import dotenv from 'dotenv';
 dotenv.config();
 
-export const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/PhoneBook',
+//формируем строку подключения из данных окружения
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/PhoneBook',
       HOST = process.env.HOST || 'localhost',
       PORT = process.env.PORT || 9000;
 
@@ -15,18 +16,19 @@ export const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/Pho
   .then(() => console.log(`🚀  Database started ${MONGO_URI}`))
   .catch(err => console.error(err))
 
-
+//массив с демо данными
 const array = [
     {number: "1111", name: "Дарт Вейдер"},
     {number: "2222", name: "Люк Скайуокер"},
     {number: "3333", name: "Падме Амидала"},
 ];
 
+//добавляем данные в базу
 Phone.insertMany(array)
     .then( (docs)=> {
-        console.log(docs);
-        mongoose.connection.close() ;
+        console.log(docs);//вывод добавленные документы
+        mongoose.connection.close() ;//закрываем соединение  сбазой
     })
     .catch( (err)=> {
-       console.log(err.message);
+       console.log(err.message);//если ошибка пишем в консоль
     });
